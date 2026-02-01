d8dc44d6087ac1118177658816b5879076624e3c
Mateo <mcgannod@gmail.com>
2026-02-01 15:28:11 -0600

    Added music

--- scenes/levels/game.gd
+++ scenes/levels/game.gd
@@ -2,9 +2,18 @@ extends Node3D
 
 signal game_started
 
+var audio: AudioStreamPlayer
+
+func _ready() -> void:
+	audio = $AudioStreamPlayer
+	audio.stream = preload('res://assets/audio/songs/Last breath menu_lobby.mp3')
+	audio.play()
+
 @rpc("authority", "call_local")
 func start_match():
-	
+	audio.stream = load('res://assets/audio/songs/Last Breath Arena Song 1.mp3')
+	audio.play()
+	await get_tree().create_timer(5).timeout
 	game_started.emit()
 
 
